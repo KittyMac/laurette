dbb742693cb84aaa6abae8bbd96766e859ab8068
Rocco Bowling <rocco@chimerasw.com>
2016-03-22 15:09:40 -0400

    WIP: first commit.  Laurette is generally working, but it still needs to:
    - properly detect what the language code of the .strings file is
    - make sure it handles quotes in string (since we embed the strings in code)
    - make a SetLanguageCode() method instead of exposing currentLanguage
    - make a SetLanguegCode() method which accepts a string, not the enum
    - should it make static string[]s instead of local ones?

---
+++ LauretteBuildProcess.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class LauretteBuildProcess : MonoBehaviour {
+
+	private static string OutputDirectory = "Assets/Code/";
+	
+	[MenuItem ("Laurette/Process All")]
+	static void ProcessAllStringsFiles() {
+		Laurette laurette = new Laurette ();
+
+		string[] allAssets = AssetDatabase.GetAllAssetPaths();
+		List<string> allStringsFiles = new List<string> ();
+
+		foreach (string asset in allAssets) {
+			if (asset.EndsWith (".strings")) {
+				allStringsFiles.Add (asset);
+			}
+		}
+
+		string error = null;
+		laurette.Process (allStringsFiles.ToArray(), OutputDirectory+"/Localizations.cs", out error);
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# laurette
-C#/Unity port of Laurine, a Localization code generator written for Swift.
+# Laurette
+***Localization code generator** written in vanilla C# with editor scripts for use in Unity3D. Laurette is inspired by the awesome [Laurine for Swift](https://github.com/JiriTrecak/Laurine), and serves much of the same function for C#/Unity3D.
+
+TODO: write more stuff here
