9a163662ae2a9da337da58e0bbec55269742bc82
Rocco Bowling <rocco@chimerasw.com>
2016-03-23 13:44:29 -0400

    adding the license to the build process code; force a, asset database refresh when the Localization.cs file changes

--- LauretteBuildProcess.cs
+++ LauretteBuildProcess.cs
@@ -1,4 +1,26 @@
-﻿using UnityEngine;
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2016 Rocco Bowling
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// 	copies or substantial portions of the Software.
+// 
+// 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// 	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// 	SOFTWARE.
+
+using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.IO;
@@ -20,7 +42,7 @@ public class LauretteBuildProcess : AssetPostprocessor  {
 
 		// Check if the output file exists, if not we should probably try generating it!
 		if (File.Exists (OutputDirectory + "/Localizations.cs") == false) {
-			ProcessStringsFiles ();
+			stringsFileWasChanged = true;
 		}
 
 		foreach (string str in importedAssets) {
@@ -54,5 +76,7 @@ public class LauretteBuildProcess : AssetPostprocessor  {
 
 		string error = null;
 		laurette.Process (allStringsFiles.ToArray(), OutputDirectory+"/Localizations.cs", out error);
+
+		AssetDatabase.Refresh ();
 	}
 }
