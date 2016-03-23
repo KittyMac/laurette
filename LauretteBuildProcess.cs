e5ddf3e782b6770292a3d418befefc47458d31fc
Rocco Bowling <rocco@chimerasw.com>
2016-03-22 23:32:41 -0400

    laurette should now check if the output file does not exist, and regenerate it

--- LauretteBuildProcess.cs
+++ LauretteBuildProcess.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using System.IO;
 
 public class LauretteBuildProcess : AssetPostprocessor  {
 
-	private static string OutputDirectory = "Assets/Code/";
+	private static string OutputDirectory = "Assets/";
+
+	static LauretteBuildProcess() {
+		// Check if the output file exists, if not we should probably try generating it!
+		if (File.Exists (OutputDirectory + "/Localizations.cs") == false) {
+			ProcessStringsFiles ();
+		}
+	}
 
 	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
 	{
 		bool stringsFileWasChanged = false;
+
+		// Check if the output file exists, if not we should probably try generating it!
+		if (File.Exists (OutputDirectory + "/Localizations.cs") == false) {
+			ProcessStringsFiles ();
+		}
 
 		foreach (string str in importedAssets) {
 			stringsFileWasChanged = stringsFileWasChanged | str.EndsWith (".strings");
@@ -23,19 +36,23 @@ public class LauretteBuildProcess : AssetPostprocessor  {
 		}
 
 		if (stringsFileWasChanged) {
-			Laurette laurette = new Laurette ();
-
-			string[] allAssets = AssetDatabase.GetAllAssetPaths();
-			List<string> allStringsFiles = new List<string> ();
-
-			foreach (string asset in allAssets) {
-				if (asset.EndsWith (".strings") || asset.EndsWith (".strings.txt")) {
-					allStringsFiles.Add (asset);
-				}
-			}
-
-			string error = null;
-			laurette.Process (allStringsFiles.ToArray(), OutputDirectory+"/Localizations.cs", out error);
+			ProcessStringsFiles ();
 		}
+	}
+
+	static void ProcessStringsFiles() {
+		Laurette laurette = new Laurette ();
+
+		string[] allAssets = AssetDatabase.GetAllAssetPaths();
+		List<string> allStringsFiles = new List<string> ();
+
+		foreach (string asset in allAssets) {
+			if (asset.EndsWith (".strings") || asset.EndsWith (".strings.txt")) {
+				allStringsFiles.Add (asset);
+			}
+		}
+
+		string error = null;
+		laurette.Process (allStringsFiles.ToArray(), OutputDirectory+"/Localizations.cs", out error);
 	}
 }
